f784612d411587da233b563decd7076f54e99d19
Marina Kotova <m.klementyeva@mail.ru>
2023-04-26 15:48:35 +0200

    chore: add eslint dependencies to the project

---
+++ app.js
@@ -0,0 +1,14 @@
+const PAGE = document.createElement('div');
+PAGE.classList.add('page');
+
+const HEADER = document.createElement('h1');
+HEADER.classList.add('header');
+HEADER.innerText = 'RSS Виртуальная клавиатура';
+const FORM = document.createElement('form');
+const TEXT_AREA = document.createElement('textarea');
+TEXT_AREA.classList.add('text-area');
+FORM.append(TEXT_AREA);
+
+PAGE.append(HEADER);
+PAGE.append(FORM);
+document.body.prepend(PAGE);
