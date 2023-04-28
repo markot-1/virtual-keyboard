8059f51070611d1fcf782af57c1b6a3140053cbc
Marina Kotova <m.klementyeva@mail.ru>
2023-04-28 17:21:08 +0200

    feat: add event listeners for keyboard, add animation for buttons

--- src/app.js
+++ src/app.js
@@ -1,19 +1,39 @@
-import { keyboardFill } from './modules/keyboard.js';
+/* eslint-disable import/prefer-default-export */
+import {
+  createElement, keyboardFill, TEXT_AREA, keyDownEvent, keyUpEvent,
+} from './modules/keyboard.js';
 
-const PAGE = document.createElement('div');
-PAGE.classList.add('page');
-
-const HEADER = document.createElement('h1');
-HEADER.classList.add('header');
+const PAGE = createElement('div', 'page');
+// PAGE.classList.add('page');
+const HEADER = createElement('h1', 'header');
+// HEADER.classList.add('header');
 HEADER.innerText = 'RSS Виртуальная клавиатура';
+const TEXT_BLOCK = createElement('p', 'text-block');
+TEXT_BLOCK.innerText = 'Клавиатура создана в операционной системе Windows';
+
 const FORM = document.createElement('form');
-const TEXT_AREA = document.createElement('textarea');
-TEXT_AREA.classList.add('text-area');
+// возможно перенести к клавиатуре 2 строки
 FORM.append(TEXT_AREA);
 
 const keyboardWrapper = keyboardFill();
 FORM.append(keyboardWrapper);
+FORM.append(TEXT_BLOCK);
 
 PAGE.append(HEADER);
 PAGE.append(FORM);
+
+document.body.addEventListener('keydown', keyDownEvent);
+document.body.addEventListener('keyup', keyUpEvent);
+
 document.body.prepend(PAGE);
+
+// let buttons = document.querySelectorAll('button');
+// let button;
+// // console.log(document.querySelectorAll('button'));
+// for (let i = 0; i < buttons.length; i += 1) {
+//   console.log(buttons[i]);
+//   if (buttons[i].innerHTML === 'a') {
+//     button = buttons[i];
+//   }
+//   console.log(button);
+// }
--- src/modules/keyboard.js
+++ src/modules/keyboard.js
@@ -1,7 +1,10 @@
 /* eslint-disable import/prefer-default-export */
 import { json } from './data.js';
 
-function createElement(tagName, className) {
+export const TEXT_AREA = document.createElement('textarea');
+TEXT_AREA.classList.add('text-area');
+
+export function createElement(tagName, className) {
   const elem = document.createElement(tagName);
   elem.classList.add(className);
   return elem;
@@ -13,9 +16,39 @@ export function keyboardFill() {
   for (let i = 0; i < json.length; i += 1) {
     const elem = document.createElement('button');
     elem.innerText = json[i].key;
-    elem.classList.add('button');
+    for (let j = 0; j < json[i].class.length; j += 1) {
+      elem.classList.add(json[i].class[j]);
+    }
     elem.id = json[i].code;
     keyboardWrapper.append(elem);
   }
   return keyboardWrapper;
 }
+
+const buttons = document.getElementsByTagName('button');
+
+export function keyDownEvent(event) {
+  let button;
+  for (let i = 0; i < buttons.length; i += 1) {
+    if (buttons[i].id === event.code) {
+      button = json[i];
+      buttons[i].classList.add('active');
+    }
+  }
+  if (button.class.indexOf('button_service') === -1 || button.code === 'Backspace') {
+    TEXT_AREA.innerHTML += button.key;
+    if (event.code === 'Backspace') {
+      TEXT_AREA.innerHTML = TEXT_AREA.innerHTML.slice(0, -10);
+    }
+  }
+}
+
+export function keyUpEvent(event) {
+  for (let i = 0; i < buttons.length; i += 1) {
+    if (buttons[i].id === event.code) {
+      buttons[i].classList.remove('active');
+    }
+  }
+}
+
+// if (buttons[i].classList.contains('button_service')) {}
